19ffce7955d18ac2c10786f3876356587063c7ed
Glenn Wiley <gwiley72@outlook.com>
2016-09-26 23:04:52 -0500

    namespaces and global scopes

--- FunctionsAndVariables/FunctionsAndVariables/FunctionsAndVariables.cpp
+++ FunctionsAndVariables/FunctionsAndVariables/FunctionsAndVariables.cpp
@@ -1,11 +1,28 @@
-// FunctionsAndVariables.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 
+namespace life
+{
+	auto meaning = 42;
+	auto* pm = &meaning;
+	auto& rm = meaning;
+}
 
 int main()
 {
-    return 0;
+	std::cout << "We like fuzzy pickles." << std::endl;
+
+	std::cout << life::meaning << " takes up " << 
+		sizeof(life::meaning) << " bytes" << std::endl;
+
+	*life::pm = 43;
+
+	std::cout << life::meaning << std::endl;
+
+	life::rm = 42;
+
+	std::cout << life::meaning << std::endl;
+
+	getchar();
+	return 0;
 }
 
